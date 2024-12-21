8f57229b65972fd31e79f1a4daf7f0dd515e63b1
LEGION <anasdahare2002@gmail.com>
2024-12-21 22:33:00 +0100

    first commit

---
+++ src/main/java/com/example/controlleprojet/ControlleProjetApplication.java
@@ -0,0 +1,13 @@
+package com.example.controlleprojet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlleProjetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlleProjetApplication.class, args);
+	}
+
+}
